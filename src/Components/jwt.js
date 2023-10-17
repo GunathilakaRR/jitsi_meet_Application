15f763e644e5d9384ef1c0fc8733ce722c8f461c
GunathilakaRR <rusirugunathilaka118@gmail.com>
2023-10-17 17:10:12 +0530

    Test jason web tokens on the applicaion

---
+++ src/Components/jwt.js
@@ -0,0 +1,52 @@
+import React from 'react'
+var jsonwebtoken = require('jsonwebtoken');
+var uuid = require('uuid-random');
+
+function jwt() {
+  
+    const generate = (privateKey, { id, name, email, avatar, appId, kid }) => {
+        const now = new Date()
+        const jwt = jsonwebtoken.sign({
+          aud: 'jitsi',
+          context: {
+            user: {
+              id,
+              name,
+              avatar,
+              email: email,
+              moderator: 'true'
+            },
+            features: {
+              livestreaming: 'true',
+              recording: 'true',
+              transcription: 'true',
+              "outbound-call": 'true'
+            }
+          },
+          iss: 'chat',
+          room: '*',
+          sub: appId,
+          exp: Math.round(now.setHours(now.getHours() + 3) / 1000),
+          nbf: (Math.round((new Date).getTime() / 1000) - 10)
+        }, privateKey, { algorithm: 'RS256', header: { kid } })
+        return jwt;
+      }
+      
+      /**
+       * Generate a new JWT.
+       */
+      const token = generate('my private key', {
+          id: uuid(),
+          name: "rusirugunathilaka118",
+          email: "rusirugunathilaka118@gmail.com",
+          avatar: "my avatar url",
+          appId: "vpaas-magic-cookie-39f5ab9db36441c186e4b2b4dc89bbda", // Your AppID ( previously tenant )
+          kid: "vpaas-magic-cookie-39f5ab9db36441c186e4b2b4dc89bbda/cf4c5f"
+      });
+      
+      console.log(token);
+
+
+}
+
+export default jwt
